4ba1c6e2a5be1f1dd81271587d0b55f3ac391fad
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-02-20 16:46:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+// modify the user API using mongodb change stream using socket.io.
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const app = express()
+const PORT = process.env.PORT || 5000; 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://assignment-12-93b12.web.app', 'https://ass-12-delta.vercel.app'],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send("ASSIGNMENT-10 SERVER RUNNING")
+})
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.x6oak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const usersCollections = client.db("ToDo").collection('users');
+        const blogsCollections = client.db("ToDo").collection('blogs');
+
+        // JWT SECTION
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.jwt_Secret, {
+                expiresIn: '1h'
+            })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: 'cookie created' })
+        })
+
+        app.post('/jwtlogout', async (req, res) => {
+            res
+                .clearCookie('token', {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: 'cookie cleared' })
+        })
+        // TOKEN VERIFIER
+        const verifyToken = (req, res, next) => {
+            const token = req?.cookies?.token
+            if (!token) {
+                return res.status(401).send({ message: 'Token not found to verify' })
+            }
+            jwt.verify(token, process.env.jwt_Secret, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'Unauthorization Error' })
+                }
+                req.user = decoded
+                next()
+            })
+        }
+
+        // user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollections.insertOne(user)
+            res.send(result)
+        })
+        app.get('/users', async (req, res) => {
+            const result = await usersCollections.find().toArray()
+            res.send(result)
+        })
+
+        // blogs
+        app.post('/blogs', async (req, res) => {
+            const user = req.body;
+            const result = await blogsCollections.insertOne(user)
+            res.send(result)
+        })
+        app.get('/blogs', async (req, res) => {
+            const result = await blogsCollections.find().toArray()
+            res.send(result)
+        })
+
+        
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
