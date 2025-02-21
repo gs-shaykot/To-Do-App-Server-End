08a95836603b617d73cb7e3054dc8b047dea25eb
GS SHAYKOT <shaykotkhan1916@gmail.com>
2025-02-21 10:43:12 +0600

    task delete api added

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/addTask/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await ToDoCollections.deleteOne(query);
+            res.send(result)
+        })
+
 
     } finally {
         // Ensures that the client will close when you finish/error
